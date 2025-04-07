c73fb4bcfbeaf0ea747401b934235b1f769b4270
Jose <joseedusolsona@gmail.com>
2025-04-07 19:14:20 +0200

    Add project files.

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,27 @@
+﻿using Prac2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prac2.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult PeopleList()
+        {
+            List<People> people = new List<People>
+            {
+                new People { StudentNumber = 1234567, FirstName = "Jose", LastName = "Edu", Email = "jose@tuks.co.za", MyLink = "~/HTML/Person1Page.html" },
+                new People { StudentNumber = 12345678, FirstName = "Jane", LastName = "Doe", Email = "jane.doe@tuks.co.za", MyLink = "~/HTML/Person2Page.html" },
+                new People { StudentNumber = 23708795, FirstName = "John", LastName = "Doe", Email = "john.doe@tuks.co.za", MyLink = "~/HTML/Person3Page.html" },
+                new People { StudentNumber = 18345689, FirstName = "Jeff", LastName = "Rizal", Email = "jeff@gmail.com", MyLink = "~/HTML/Person4Page.html" },
+                new People { StudentNumber = 28658477, FirstName = "Andres", LastName = "Bonifacio", Email = "Andres@gmail.com", MyLink = "~/HTML/Person5Page.html" }
+            };
+
+            return View(people);
+        }
+    }
+}
---
+++ Models/People.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Prac2.Models
+{
+	public class People
+	{
+        [Display(Name = "Student Number")]
+        public int StudentNumber { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Profile Link")]
+        public string MyLink { get; set; }
+    }
+}
